d1e2a6bd990a2eb327b675bc682da51c181e6acc
你的名字或Jacky <Email>
2026-06-06 17:45:26 +0800

    顯示訂單建立 付款 時間點

--- src/app/(protected)/account/subscription/_components/utils.ts
+++ src/app/(protected)/account/subscription/_components/utils.ts
@@ -8,6 +8,9 @@ const dateFormatter = new Intl.DateTimeFormat("zh-TW", {
   year: "numeric",
   month: "long",
   day: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
+  hour12: false,
 });
 
 export function formatCurrency(amount: number) {
